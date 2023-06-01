d225fca6de4ee8ade4cf2256db778c0f01b74756
Bright Qi <brightq@gmail.com>
2023-06-01 16:55:03 +0800

    module 3 3.6 use Plagiarism Check API on scanwebhook

--- pages/api/copy-leaks/completed/[scanId].ts
+++ pages/api/copy-leaks/completed/[scanId].ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import { CopyLeaksWrapper } from "@/lib/copy-leaks/copyLeaksWrapper";
 
 export const config = {
   runtime: "edge",
@@ -31,22 +32,25 @@ function getHighestSourceResult(
 }
 
 export default async function handler(req: NextRequest) {
-  const body = await req.json();
-  const scanId = body.scannedDocument.scanId;
-  const matchedWords = getHighestSourceResult(body);
-
-  try {
-    await fetch(
-      `${process.env.NEXT_PUBLIC_FIREBASE_REALTIME_DATABASE_URL}/scans/${scanId}.json`,
-      {
-        method: "PUT",
-        body: JSON.stringify({ matchedWords: matchedWords }),
-      }
-    );
-  } catch (e) {
-    console.error("Error writing to Firebase Database", e);
-    throw e;
+    const body = await req.json();
+    const scanId = body.scannedDocument.scanId;
+    const { resultId, matchedWords } = getHighestSourceResult(body);
+    if (matchedWords != 0) {
+      const copyLeaks = new CopyLeaksWrapper();
+      await copyLeaks.getDetailedResults(scanId, resultId);
+    }
+    try {
+      await fetch(
+        `${process.env.NEXT_PUBLIC_FIREBASE_REALTIME_DATABASE_URL}/scans/${scanId}.json`,
+        {
+          method: "PUT",
+          body: JSON.stringify({ matchedWords: matchedWords }),
+        }
+      );
+    } catch (e) {
+      console.error("Error writing to Firebase Database", e);
+      throw e;
+    }
+  
+    return NextResponse.json({ message: "Scan complete" });
   }
-
-  return NextResponse.json({ message: "Scan complete" });
-}
